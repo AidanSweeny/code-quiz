db357710ff2c7cceec0454b6ad14235adfd0861a
Aidan Sweeny <apswee22@colby.edu>
2020-06-21 14:43:59 -0400

    Added random generator to choose a question

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ var answer1 = document.querySelector(".answer1");
 var answer2 = document.querySelector(".answer2");
 var answer3 = document.querySelector(".answer3");
 var answer4 = document.querySelector(".answer4");
-var questions = [{q:"question?", a1:"answer" , a2:"answer", a3:"answer", a4:"answer"}];
+var questions = [{q:"question?", a1:"answer" , a2:"answer", a3:"answer", a4:"answer"}, {q:"question?", a1:"answer" , a2:"answer", a3:"answer", a4:"answer"}, {q:"question?", a1:"answer" , a2:"answer", a3:"answer", a4:"answer"}, {q:"question?", a1:"answer" , a2:"answer", a3:"answer", a4:"answer"}, {q:"question?", a1:"answer" , a2:"answer", a3:"answer", a4:"answer"}];
 
 function startQuiz(event) {
+    startBtn.style.display = "none";
+    startTag.style.display = "none";
+    answer1.style.display = "block";
+    answer2.style.display = "block";
+    answer3.style.display = "block";
+    answer4.style.display = "block";
     var timerInterval = setInterval(function() {
-        startBtn.style.display = "none";
-        startTag.style.display = "none";
-        answer1.style.display = "block";
-        answer2.style.display = "block";
-        answer3.style.display = "block";
-        answer4.style.display = "block";
-
+        var num = Math.floor(Math.random() * 5)
+        answer1.textContent = questions[num].a1;
+        answer2.textContent = questions[num].a2;
+        answer3.textContent = questions[num].a3;
+        answer4.textContent = questions[num].a4;
     }, 1000);
 }
 
