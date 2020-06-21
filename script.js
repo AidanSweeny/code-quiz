b642aa6d17349096ccd638b93f4f6df27af8bd71
Aidan Sweeny <apswee22@colby.edu>
2020-06-21 19:53:41 -0400

    Working on selecting correct answer

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var answers = document.querySelector(".answers");
 var timer = document.querySelector(".timer");
 var questions = [{q:"What type bender is Katara?", a1:"air" , a2:"water", a3:"earth", a4:"fire", c: "water"}, {q:"What does Sokka's girlfriend turn into?", a1:"a fish" , a2:"the sun", a3:"the moon", a4:"a bear", c:"the moon"}, {q:"question?", a1:"answer" , a2:"answer", a3:"answer", a4:"answer"}, {q:"question?", a1:"answer" , a2:"answer", a3:"answer", a4:"answer"}, {q:"question?", a1:"answer" , a2:"answer", a3:"answer", a4:"answer"}];
 var time = 0;
+var num = Math.floor(Math.random() * questions.length);
 
 function startQuiz(event) {
     startBtn.style.display = "none";
@@ -17,7 +18,6 @@ function startQuiz(event) {
     answer2.style.display = "block";
     answer3.style.display = "block";
     answer4.style.display = "block";
-    var num = Math.floor(Math.random() * questions.length);
     answer1.textContent = questions[num].a1;
     answer2.textContent = questions[num].a2;
     answer3.textContent = questions[num].a3;
@@ -28,8 +28,10 @@ function startQuiz(event) {
     }, 1000);
 }
 
-function question(answer, num) {
-    if (answer === questions[num]){
+function question() {
+    console.log(event.target);
+    console.log(num);
+    if (event.target === questions[num]){
         alert("correct");
     }
     else {
