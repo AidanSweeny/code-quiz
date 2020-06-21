510a09206fdf5e28576d2c06bb5788013944ef46
Aidan Sweeny <apswee22@colby.edu>
2020-06-21 13:59:26 -0400

    Added html buttons and query selectors

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 var startBtn = document.querySelector("startBtn");
-
+var startTag = document.querySelector("start");
+var questionTag = document.querySelector("question");
+var answer1 = document.querySelector("answer1");
+var answer2 = document.querySelector("answer2");
+var answer3 = document.querySelector("answer3");
+var answer4 = document.querySelector("answer4");
+var questions = [{q:"question?", a1:"answer",j , a2:"answer", a3:"answer", a4:"answer"}];
 
 function startQuiz(event) {
-
+    
 }
 
 startBtn.addEventListener("click", startQuiz);
