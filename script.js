5cb3cecec0831f1d31dbec4a93281297e38f0df0
Aidan Sweeny <apswee22@colby.edu>
2020-06-23 19:23:23 -0400

    Added final touches to highscores

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var answer3 = document.querySelector(".answer3");
 var answer4 = document.querySelector(".answer4");
 var answers = document.querySelector(".answers");
 var yesOrNo = document.querySelector(".yesOrNo");
+var scorers = document.querySelector(".scorers");
+var close = document.querySelector(".close");
 var initialBtn = document.querySelector(".initialBtn");
 var timer = document.querySelector(".timer");
 var scorer = document.querySelector(".score");
@@ -29,15 +31,31 @@ var questionNum = Math.floor(Math.random() * questions.length);
 var score = 0;
 
 function startQuiz(event) {
+    questions = [{q:"What type bender is Katara?", a1:"air" , a2:"water", a3:"earth", a4:"fire", c: "water"}, 
+{q:"What does Sokka's girlfriend turn into?", a1:"a fish" , a2:"the sun", a3:"the moon", a4:"a bear", c:"the moon"}, 
+{q:"What type of earth does Toph learn to bend?", a1:"magma" , a2:"diamond", a3:"metal", a4:"mercury", c:"metal"}, 
+{q:"Who was the avatar befor Aang?", a1:"Roku" , a2:"Kyoshi", a3:"Sokka", a4:"Korra", c:"Roku"}, 
+{q:"Who taught Zuko and Aang to firebend?", a1:"Iroh" , a2:"Dragons", a3:"The Fire Lord", a4:"The Sun", c: "Dragons"}, 
+{q:"What do you say to make Appa fly?", a1:"hoo-ha" , a2:"yee-ha", a3:"yipee", a4:"yip-yip", c: "yip-yip"},
+{q:"What does Aang use to heal Sokka and Katara when they are sick?", a1:"cough syrup" , a2:"frozen frogs", a3:"candy", a4:"burnt leaves", c: "frozen frogs"},
+{q:"What does Sokka make his sword from?", a1:"metal" , a2:"wood", a3:"stone", a4:"meteorite", c: "meteorite"}, 
+{q:"What is the organization that Iroh is a part of?", a1:"The Jasmine Dragon" , a2:"The Gold Leaf", a3:"The White Lotus", a4:"Fire Benders Anonymous", c: "The White Lotus"}, 
+{q:"Who must Aang let go of to unlock the Avatar state?", a1:"Sokka" , a2:"Toph", a3:"Roku", a4:"Katara", c: "Katara"}];
     score = 0;
     time = 60;
     startBtn.style.display = "none";
+    viewHighScores.style.display = "none";
+    scorer.style.display = "none";
+    initialBtn.style.display = "none";
+    namer.style.display = "none";
     startTag.style.display = "none";
     answer1.style.display = "block";
     answer2.style.display = "block";
     answer3.style.display = "block";
     answer4.style.display = "block";
+    timer.style.display = "block";
     questionTag.style.display = "block";
+    highScore.style.display = "none";
     timer.textContent = time;
     questionTag.textContent = questions[questionNum].q;
     answer1.textContent = questions[questionNum].a1;
@@ -53,6 +71,7 @@ function startQuiz(event) {
             startTag.style.display = "block";
             startTag.textContent = "Game Over"
             answer1.style.display = "none";
+            viewHighScores.style.display = "block";
             answer2.style.display = "none";
             answer3.style.display = "none";
             answer4.style.display = "none";
@@ -69,8 +88,6 @@ function startQuiz(event) {
 }
 
 function question(event) {
-    console.log(questions[questionNum]["c"])
-    console.log(event.target.textContent)
     if (event.target.textContent === questions[questionNum]["c"]){
         yesOrNo.style.display = "block";
         yesOrNo.textContent = "Correct!";
@@ -86,7 +103,6 @@ function question(event) {
     }
 
     questions.splice(questionNum,1);
-    console.log(questions)
     var newNum = Math.floor(Math.random() * questions.length);
     if(questions.length === 0){
         return;
@@ -102,7 +118,6 @@ function question(event) {
 startBtn.addEventListener("click", startQuiz);
 answers.addEventListener("click", question);
 highScore.addEventListener("click", function(){
-    startTag.style.display = "none";
     startBtn.style.display = "none";
     initialBtn.style.display = "block";
     highScore.style.display = "none";
@@ -110,5 +125,33 @@ highScore.addEventListener("click", function(){
 });
 initialBtn.addEventListener("click", function(){
     highScores[namer.value] = score;
+    initialBtn.style.display = "none";
+    namer.style.display = "none";
+    startBtn.textContent = "Start Again?";
+    startBtn.style.display = "block";
+    scorer.style.display = "none";
+
 });
 
+viewHighScores.addEventListener("click", function(){
+    close.style.display = "block";
+    initialBtn.style.display = "none";
+    namer.style.display = "none";
+    startBtn.style.display = "none";
+    startTag.style.display = "none";
+    scorer.style.display = "none";
+    var keys = Object.keys(highScores);
+    var values = Object.values(highScores);
+    for (var i=0; i<Object.keys(highScores).length; i++){
+        var li = document.createElement("li");
+        li.textContent ="Initials: " + keys[i]+ " Score: " + values[i];
+        scorers.appendChild(li);
+    }
+});
+
+close.addEventListener("click", function(){
+    startBtn.style.display = "block";
+    startTag.style.display = "block";
+    scorers.style.display = "none";
+    close.style.display = "none";
+});
